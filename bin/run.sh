698a9627035df51481ebd6cd84026e6152776612
Jakub <jakub.beno@pjwstk.edu.pl>
2025-06-25 22:19:30 +0200

    Add bin

---
+++ bin/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+TKZ_PATH="$(dirname "$0")"
+java --module-path "$TKZ_PATH/lib/BackupStrategies.jar:$TKZ_PATH/TKZ.jar:$TKZ_PATH/lib/GUI.jar:$TKZ_PATH/lib/APIUtils.jar:$TKZ_PATH/lib/IconsStrategies.jar:$TKZ_PATH/lib/ProfileManagement.jar" -m TKZ/ApplicationExecution.Main
