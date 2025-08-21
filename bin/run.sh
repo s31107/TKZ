8ad823edc1fe35b48c8dc97a9e0ea441d5971254
Jakub <jakub.beno@pjwstk.edu.pl>
2025-08-21 17:33:00 +0200

    Fix run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 TKZ_PATH="$(dirname "$0")"
-java24 --module-path "$TKZ_PATH/lib/BackupStrategies.jar:$TKZ_PATH/TKZ.jar:$TKZ_PATH/lib/GUI.jar:$TKZ_PATH/lib/APIUtils.jar:$TKZ_PATH/lib/IconsStrategies.jar:$TKZ_PATH/lib/ProfileManagement.jar" -m TKZ/ApplicationExecution.Main
+java --module-path "$TKZ_PATH/lib/BackupStrategies.jar:$TKZ_PATH/TKZ.jar:$TKZ_PATH/lib/GUI.jar:$TKZ_PATH/lib/APIUtils.jar:$TKZ_PATH/lib/IconsStrategies.jar:$TKZ_PATH/lib/ProfileManagement.jar" -m TKZ/ApplicationExecution.Main
